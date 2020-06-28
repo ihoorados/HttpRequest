01eff9351fcf535c6fa96eb26d76c534aac93b7c
Hoorad Ramezani <hooradramezani@gmail.com>
2020-06-28 20:25:03 +0430

    Initial Commit

---
+++ HttpRequest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HttpRequest
+//
+//  Created by Hoorad on 6/28/20.
+//  Copyright © 2020 Hoorad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
