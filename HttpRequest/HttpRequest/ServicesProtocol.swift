9805b04ab8882d48782dbf77002e0b22741044af
Hoorad Ramezani <hooradramezani@gmail.com>
2020-07-01 18:06:20 +0430

    +Service Network Layer +Service Protocol +Service Request +Service Controller

--- HttpRequest/HttpRequest/ServicesProtocol.swift
+++ HttpRequest/HttpRequest/ServicesProtocol.swift
@@ -9,21 +9,5 @@
 import Foundation
 
 protocol ServiceProtocol {
-    func post(_ request: Request, completion: (Data, Error) -> Void)
     func get(_ request: Request,completion: (Data, Error) -> Void)
-}
-
-protocol GetRequestProtocol {
-    var URL: URL { get }
-    var path: String { get }
-    var method: HTTPMethod { get }
-    var headers: [Headers] { get }
-}
-
-protocol PostRequestProtocol {
-    var URL: URL { get }
-    var path: String { get }
-    var method: HTTPMethod { get }
-    var headers: [Headers] { get }
-    var parameter : [String:String] { get }
 }
