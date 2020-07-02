cac7175817ed48b46592b37201365d87ce6ae895
Hoorad Ramezani <hooradramezani@gmail.com>
2020-07-03 01:39:18 +0430

    add main model file

--- HttpRequest/HttpRequest/ServiceRequest.swift
+++ HttpRequest/HttpRequest/ServiceRequest.swift
@@ -26,4 +26,3 @@ struct Request{
     var headers: [Headers]?
 }
 
-
---
+++ HttpRequest/Main/Model.swift
@@ -0,0 +1,9 @@
+//
+//  Model.swift
+//  HttpRequest
+//
+//  Created by Hoorad on 7/3/20.
+//  Copyright © 2020 Hoorad. All rights reserved.
+//
+
+import Foundation
