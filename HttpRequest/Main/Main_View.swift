2d22574883459542d7ac38bb24781a96e979ae81
Hoorad Ramezani <hooradramezani@gmail.com>
2020-07-20 18:21:10 +0430

    update values

--- HttpRequest/Main/Main_View.swift
+++ HttpRequest/Main/Main_View.swift
@@ -22,7 +22,7 @@ class MainView: UIViewController {
     lazy var CancelRequestBtn:UIButton = {
         let button = UIButton()
         button.layer.cornerRadius = 12
-        button.backgroundColor = #colorLiteral(red: 0.9529411765, green: 0.9568627451, blue: 0.9607843137, alpha: 1)
+        button.backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
         button.setTitleColor(#colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1), for: .normal)
         button.setTitleColor(#colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1), for: .highlighted)
         button.setTitle("Cancel Request", for: .normal)
@@ -53,7 +53,7 @@ class MainView: UIViewController {
         SendRequestBtn.translatesAutoresizingMaskIntoConstraints = false
         SendRequestBtn.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -46.0).isActive = true
         SendRequestBtn.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 46.0).isActive = true
-        SendRequestBtn.bottomAnchor.constraint(equalTo: SendRequestBtn.topAnchor, constant: -32).isActive = true
+        SendRequestBtn.bottomAnchor.constraint(equalTo: CancelRequestBtn.topAnchor, constant: -32).isActive = true
         SendRequestBtn.heightAnchor.constraint(equalToConstant: 50).isActive = true
         SendRequestBtn.addTarget(self, action: #selector(SendRequest), for: .touchUpInside)
         
