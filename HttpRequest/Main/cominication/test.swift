26793ce31cb882214a65c5116bb42304934c855c
Hoorad Ramezani <hooradramezani@gmail.com>
2020-08-30 23:02:12 +0430

    t1

---
+++ HttpRequest/Main/cominication/test.swift
@@ -0,0 +1,25 @@
+//
+//  test.swift
+//  HttpRequest
+//
+//  Created by Hoorad on 8/30/20.
+//  Copyright © 2020 Hoorad. All rights reserved.
+//
+
+import Foundation
+
+protocol Endpoint {
+    
+    associatedtype Unpacker
+    
+    var baseURL: URL {  get  }
+    var path: String {  get  }
+    var method: HTTPMethod {  get  }
+    
+    
+}
+
+
+protocol Communicator {
+    
+}
